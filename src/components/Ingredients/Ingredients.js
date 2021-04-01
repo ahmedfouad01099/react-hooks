49d6a6c8a45bc822a457a3626814060f7722ceaf
Ahmed Fouad <ahmedfouad01099@gmail.com>
2021-04-01 17:20:05 +0200

    adding useState & useEffect react hooks

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
@@ -7,18 +7,53 @@ import Search from "./Search";
 const Ingredients = () => {
   const [userIngredient, setUserIngredients] = useState([]);
 
+  // this is rendered whenever this component is rerendered
+  // after and for every render cycle
+
+  // the second argument is the depenencies
+  // when using an empty array like this [] as a second argument, useEffect() acts like componentDidMount()
+  // it runs ONLY ONCE (after the first render)
+  // if we doesn't pass a second argument with [] it will acts like componetDidUpdate()
+  // and will get into infinite loop with the request you are sending
+
+  // you could use multiple use effect as many as you want in your component
+  // as we don't speciefiy a second argument here this will loged 2 times as we rendering this component twice
+  // when we use setUserIngredients
+  useEffect(() => {
+    console.log("RENDERING INGREDINETS", userIngredient);
+  }, [userIngredient]);
+
+  // when using useCallbake function you pass first the function and the second argument
+  // is the dependencies of this function & this function has no dependecies so we will pass []
+  const filterIngredientHandler = useCallback((fileterIngredients) => {
+    setUserIngredients(fileterIngredients);
+  }, []);
+
   const addIngredientHandler = (ingredient) => {
-    setUserIngredients((prevIngredients) => [
-      ...prevIngredients,
-      { id: Math.random().toString(), ...ingredient },
-    ]);
+    fetch(
+      "https://react-hooks-18607-default-rtdb.firebaseio.com/ingredients.json",
+      {
+        method: "POST",
+        body: JSON.stringify(ingredient),
+        headers: { "Content-Type": "application/json" },
+      }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((resData) => {
+        setUserIngredients((prevIngredients) => [
+          ...prevIngredients,
+          { id: resData.name, ...ingredient },
+        ]);
+      });
   };
   return (
     <div className="App">
       <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
-        <Search />
+        <Search onLoadIngredients={filterIngredientHandler} />
         <IngredientList ingredients={userIngredient} onRemoveItem={() => {}} />
       </section>
     </div>
